a6311f6c687831f0333e4a6530faa750a6b84ad4
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2018-06-04 20:58:49 +0900

    Fix app.js
    Final version app.js

--- js/app.js
+++ js/app.js
@@ -8,34 +8,15 @@ const restart = document.querySelector('.restart');
 const deck = document.querySelector('.deck');
 //card's correctly matching number
 const moves = document.querySelector('.moves');
-//star element. I don't understand role of this.
-//const star = document.querySelector('.fa-star');
-
+//star element.
+//const stars = document.querySelector('.stars');
+const stars = document.querySelector('.stars');
+//container element.
 const container = document.querySelector('.container');
 //count makes to move new page if all cards are matched
 let count = 0;
-
-//failed event : function shuffle cards to add shuffle event
-// 01
-// function randomDeck () {
-//   for (const card of cards) {
-//     deck.appendChild(card);
-//   }
-// }
-//02
-// function randomDeck () {
-//   shuffle(cards);
-//   for (const card of cards) {
-//     if (card.childNodes[1].className === 'open') {
-//       card.childNodes[1].classList.remove('open');
-//     } else if (card.childNodes[1].className === 'show') {
-//       card.childNodes[1].classList.remove('show');
-//     } else {
-//       card.childNodes[1].classList.remove('match');
-//     }
-//   }
-//   moves.innerHTML = 0;
-// }
+//starCount helps to change stars.
+let starCount = 0;
 
 //Add event : shuffle cards
 
@@ -52,6 +33,14 @@ restart.addEventListener('click', function (e) {
     deck.appendChild(card);
   }
   moves.innerHTML = 0;
+  count = 0;
+  starCount = 0;
+  if (finalStar === 2) {
+    stars.children[0].innerHTML = `<i class="fa fa-star"></i>`;
+  } else if (finalStar === 1) {
+    stars.children[0].innerHTML = `<i class="fa fa-star"></i>`;
+    stars.children[1].innerHTML = `<i class="fa fa-star"></i>`;
+  }
 });
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -68,15 +57,17 @@ function shuffle(array) {
 
     return array;
 }
+
 /* I used setTimeout method to give enough time to have animations. */
 //Add event : open and close card
 deck.addEventListener('click', function (ev) {
   ev.preventDefault();
   if ((ev.target.className !== 'open') || (ev.target.className !== 'match')) {
     ev.target.classList.add('open', 'show');
-    // ev.target.style.animationName = 'click';
+    deck.style.pointerEvents = 'none';
     //get li that className is 'card open show' and give setTimeout. It needs time that second card have animation.
     setTimeout(function () {
+
       if (show.length === 2) {
         if(show[0].children[0].classList.value === show[1].children[0].classList.value) {
           //change show's first element
@@ -86,23 +77,26 @@ deck.addEventListener('click', function (ev) {
           //change moves
           moves.innerHTML = parseInt(moves.innerText) + 1;
           count += 1;
+          starCount += 1;
+          starGrade();
           if (count === 8) {
+            stopTime();
             setTimeout(function () {success();}, 2000);
           }
         } else {
-
           setTimeout(function ()
-            { falseClass(ev.target) }, 800);
+            { falseClass(ev.target) }, 700);
           setTimeout(function ()
-            { falseClass(show[0]) }, 800);
+            { falseClass(show[0]) }, 700);
           //change moves
           moves.innerHTML = parseInt(moves.innerText) + 1;
-
+          starCount += 1;
+          starGrade();
         }
       }
-    }, 1600);
+      deck.style.pointerEvents = 'auto';
+    }, 1100);
   }
-
 });
 
 //Add event : matching
@@ -111,6 +105,7 @@ deck.addEventListener('click', function (ev) {
 function trueClass (target) {
   target.classList.add('match');
   target.classList.remove('show', 'open');
+
 }
 //change cards if they aren't same
 function falseClass (target) {
@@ -118,53 +113,91 @@ function falseClass (target) {
   target.classList.add('wrong');
   setTimeout(function () {
     target.classList.remove('wrong');
-  }, 800);
+  }, 700);
 }
 
-//failed function : matching
-// function matching (targets) {
-//   if(show[0].childNodes[1].className === show[1].childNodes[1].className) {
-//     trueClass(show[0]);
-//     trueClass(show[1]);
-//     moves.innerHTML = parseInt(moves.innerText) + 1
-//
-//   } else {
-//     falseClass(show[0]);
-//     falseClass(show[1]);
-//   }
-// }
+//It will be used to show timer.
+let second = 0;
+let minute = 0;
+let timeCheck = setInterval(timer, 1000);
+//function : timer
+function timer () {
+  second += 1;
 
-//It will be used to give value at resultPage.
-let score = parseInt(moves.innerText);
+  if (second === 60) {
+    minute += 1;
+    second = 0;
+  }
+  let time = `${minute}:${second}`;
+  document.querySelector('.timer').innerHTML = time;
+}
+function stopTime () {
+  clearInterval(timeCheck);
+}
+
+/* function : star grade. if moves are higher than 8, stars will change colors.
+For test, I reduced starCount number in switch statement to case 3, case 5.*/
+//This star is white star. It will be used to change <ul class="star">
+const newStar = `<i class="fa fa-star-o"></i>`;
+//This variable gives value to the final score page.
+let finalStar = 0;
+function starGrade () {
+  switch (starCount) {
+    case 3:
+      console.log(starCount);
+      stars.children[0].innerHTML = newStar;
+      finalStar = 2
+      break;
+    case 5:
+      console.log(starCount);
+      stars.children[1].innerHTML = newStar;
+      finalStar = 1
+      break;
+  }
+}
 
 //function that appearing message when all cards are matched.
 function successMessage () {
-  let resultPage = '';
+  //It helps to give value at resultPage.
   let score = parseInt(moves.innerText);
-  resultPage += `<div class="success" style="text-align:center; width:300px; height:300px; margin-left: 800px;">
-  <h1 class="message">Congratulation!!</h1>
-  <p class="message">You have ${score} moves to win the game!</p>
-  <button class="restart" onclick="restartButton()">Restart</button>`;
-  document.body.innerHTML = resultPage;
+  // let score = parseInt(moves.innerText);
+  document.body.innerHTML = '';
+  swal({
+    title: 'Success!',
+    text: `You have ${score} moves and ${finalStar} stars left!`,
+    type: 'success',
+    confirmButtonColor: '#3085d6',
+    confirmButtonText: 'Try again'
+  }).then((result) => {
+    if (result.value) {
+      restartButton();
+    }
+  });
 }
-
+//It makes to reset stars element.
+const oldStar = `<i class="fa fa-star"></i>`;
 //function that restart memory game again.
-/* I used this loop again because restartButton() excutes shuffle(arrayCards). It needs to input shuffled cards to HTML.
-This codes has problem. It sometimes makes error when two cards are not matched.
- (Error is that moves add two counts, not one. And Console says "Uncaught TypeError: Cannot read property 'classList' of undefined
-  at falseClass (app.js:117) at app.js:97)
-But strange thing is that this restartButton works well sometimes. I can't find the reason. Why this problem happes? */
 function restartButton () {
   document.body.innerHTML = '';
   shuffle(arrayCards);
-  //note. this part sometimes makes error.
-  // for (const card of arrayCards) {
-  //   card.classList.remove('open', 'show', 'match');
-  //   deck.appendChild(card);
-  // }
-  // moves.innerHTML = 0;
 
+  for (const card of arrayCards) {
+    card.classList.remove('open', 'show', 'match');
+    deck.appendChild(card);
+  }
+  //reset moves, starCount, minute, second
+  moves.innerHTML = 0;
+  count = 0;
+  starCount = 0;
+  minute = 0;
+  second = 0;
+  //reset star elements
+  stars.children[0].innerHTML = oldStar;
+  stars.children[1].innerHTML = oldStar;
+
+  setInterval(timer, 1000);
   document.body.appendChild(container);
+
 }
 
 //success function that starts successMessage function.
