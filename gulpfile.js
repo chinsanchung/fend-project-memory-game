71d17310689e50978babfb26a4bde029c55600b9
Chinsan Chung <33484830+chinsanchung@users.noreply.github.com>
2018-08-31 15:01:01 +0900

    Update gulpfile.js
    Fix default codes and add ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var eslint = require('gulp-eslint');
+
+gulp.task('default', ['sass', 'lint'], function() {
+  gulp.watch('styles/sass/**/*.scss', ['sass'])
+  gulp.watch('js/**/*.js', ['lint'])
+});
 
 gulp.task('sass', function() {
-  return gulp.src('styles/sass/**/*.scss')
+  gulp.src('styles/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions']
@@ -13,6 +19,9 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('styles/css'));
 });
 
-gulp.task('sass:watch', function() {
-  gulp.watch('styles/sass/**/*.scss', ['sass']);
+gulp.task('lint', function() {
+  return gulp.src(['js/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
